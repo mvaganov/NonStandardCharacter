467a401474da696bb1f38e71d46883513b426dbf
Michael Vaganov <mvaganov@hotmail.com>
2021-12-14 23:26:31 -0800

    input fixes

--- Scripts/NonStandardUnity/Character/CharacterInputAutomate.cs
+++ Scripts/NonStandardUnity/Character/CharacterInputAutomate.cs
@@ -1,8 +1,9 @@
 using NonStandard.Character;
-using NonStandard.Process;
 using NonStandard.Utility.UnityEditor;
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 #if !ENABLE_INPUT_SYSTEM
 public class CharacterInputAutomate : CharacterInputLegacy { }
@@ -13,11 +14,29 @@ public class CharacterInputAutomate : MonoBehaviour {
     public InputActionAsset inputActionAsset;
     [Tooltip("if not null, this is where input related scripts will be added and modified.")]
     public Transform playerInputObject;
-    GameObject internalIinputObject = null;
     [Tooltip("Where the player should get input from. If null (expected), will generate PlayerInput script here.")]
     public PlayerInput playerInput;
     const string MouseLookActionMapName = "MouseLook";
     InputActionMap mouselookActionMap;
+
+    public InputActionBindings[] inputBindings;
+    bool initialized = false;
+
+#if UNITY_EDITOR
+    private void Reset() {
+        CharacterMove cm = GetComponent<CharacterMove>();
+        CharacterCamera cc = GetComponentInChildren<CharacterCamera>();
+        if (cc != null && !cc.IsTargettingChildOf(cm.transform)) { cc = null; }
+        if (cc == null) { cc = CharacterCamera.FindCameraTargettingChildOf(cm.transform); }
+        inputBindings = new InputActionBindings[] {
+            new InputActionBindings("Move", "Vector2", new EventBind(cm, nameof(cm.SetMove))),
+            new InputActionBindings("Jump", "Button", new EventBind(cm, nameof(cm.SetJump))),
+            new InputActionBindings("Toggle MouseLook", "Button", new EventBind(this, nameof(BindMouselookInputMapToButton))),
+            new InputActionBindings("MouseLook", "Vector2", new EventBind(cc, nameof(cc.ProcessLookRotation)))
+        };
+    }
+#endif
+
     void Start() {
         if (playerInputObject == null) {
             playerInputObject = transform;
@@ -25,75 +44,94 @@ public class CharacterInputAutomate : MonoBehaviour {
         if (playerInput == null) {
             playerInput = GetComponent<PlayerInput>();
             if (playerInput == null) {
-                internalIinputObject = new GameObject("player input");
-                internalIinputObject.transform.SetParent(playerInputObject);
-                playerInput = internalIinputObject.AddComponent<PlayerInput>();
+                // INTENTIONALLY NOT CREATING A PlayerInput OBJECT. PlayerInput is buggy, and won't update it's action
+                // events dynamically, except in edit mode when the UI is focused on the PlayerInput object.
+                //playerInput = playerInputObject.gameObject.AddComponent<PlayerInput>();
             }
-            playerInput.notificationBehavior = PlayerNotifications.InvokeUnityEvents;
+            if (playerInput != null) {
+                playerInput.notificationBehavior = PlayerNotifications.InvokeUnityEvents;
+            }
         } else {
             if (inputActionAsset == null) {
                 inputActionAsset = playerInput.actions;
             }
         }
-        if (playerInput.actions == null && playerInput.notificationBehavior == PlayerNotifications.InvokeUnityEvents) {
-            //playerInput.onControlsChanged -= MakeSureCharacterMoveBindsAreSet;
-            //playerInput.onControlsChanged += MakeSureCharacterMoveBindsAreSet;
-            playerInput.actions = inputActionAsset;
-            //playerInput.DeactivateInput();
-            //playerInput.ActivateInput();
-        //    Proc.Delay(10000, () => MakeSureCharacterMoveBindsAreSet(playerInput));
-        //} else {
-        //    MakeSureCharacterMoveBindsAreSet(playerInput);
-            if(internalIinputObject != null) {
-                GameObject tryItAgainBecausePlayerInputWontRefreshActionEvents = Instantiate(internalIinputObject);
-                Destroy(internalIinputObject);
-                internalIinputObject = tryItAgainBecausePlayerInputWontRefreshActionEvents;
-                internalIinputObject.transform.SetParent(playerInputObject);
-                playerInput = internalIinputObject.GetComponent<PlayerInput>();
-            }
+        mouselookActionMap = inputActionAsset.FindActionMap(MouseLookActionMapName);
+        if (mouselookActionMap == null) {
+            throw new Exception($"character controls need a `{MouseLookActionMapName}` action map");
         }
-        StartCoroutine(MakeSureCharacterMoveBindsAreSet(playerInput));
+        if (playerInput == null) {
+            BindDirectlyWithoutPlayerInput(inputBindings, true);
+        } else {
+            StartCoroutine(MakeSureCharacterMoveBindsAreSet(playerInput));
+        }
     }
+    /// <summary>
+    /// the <see cref="PlayerInput"/> class updates it's actionEvents during Editor time, so it can't be used dynamically.
+    /// </summary>
+    /// <param name="pi"></param>
+    /// <returns></returns>
     IEnumerator MakeSureCharacterMoveBindsAreSet(PlayerInput pi) {
-        CharacterMove cm = GetComponent<CharacterMove>();
-        CharacterCamera cc = GetComponentInChildren<CharacterCamera>();
-        if (cc != null && !cc.IsTargettingChildOf(cm.transform)) { cc = null; }
-        if (cc == null) { cc = CharacterCamera.FindCameraTargettingChildOf(cm.transform); }
-        Inpt[] inputs = new Inpt[] {
-            new Inpt("Move", "Vector2", new EventBind(cm, nameof(cm.SetMove))),
-            new Inpt("Jump", "", new EventBind(cm, nameof(cm.SetJump))),
-            new Inpt("Toggle MouseLook", "", new EventBind(this, nameof(BindMouselookInputMapToButton))),
-            new Inpt("MouseLook", "Vector2", new EventBind(cc, nameof(cc.ProcessLookRotation)))
-        };
-        mouselookActionMap = playerInput.actions.FindActionMap(MouseLookActionMapName);
-        if (mouselookActionMap == null) { throw new System.Exception("character controls need a `MouseLook` action map"); }
-        for (int i = 0; i < inputs.Length; ++i) {
-            if (!inputs[i].BindAction(playerInput)) {
+        int attempts = 0;
+        for (int i = 0; i < inputBindings.Length; ++i) {
+            if (!inputBindings[i].BindAction(playerInput)) {
                 attempts++;
                 if(attempts > 10000) {
                     Debug.Log("ok, that's too long.");
                     yield break;
                 }
-                Debug.Log("try again soon "+attempts);
+                //Debug.Log("try again soon "+attempts+" "+playerInput.actionEvents.Count);
                 --i;
-                internalIinputObject.gameObject.SetActive(!internalIinputObject.gameObject.activeSelf);
                 yield return null;
             }
         }
         playerInput.enabled = false;
         yield return null;
         playerInput.enabled = true;
-        //InputActionMap playerMap = null, iam;
-        //for (int i = 0; i < playerInput.actions.actionMaps.Count; ++i) {
-        //    iam = playerInput.actions.actionMaps[i];
-        //    Debug.Log("|"+iam.name+"|");
-        //    if(iam.name == "Player") { playerMap = iam; break; }
-        //}
-        //iam = playerInput.actions.FindActionMap("Player");
-        //Debug.Log("enabling "+playerMap+" "+iam);
-        //playerMap.Enable();
+        initialized = true;
     }
-    private static int attempts = 0;
+    void BindDirectlyWithoutPlayerInput(InputActionBindings[] inputs, bool enable) {
+        for (int i = 0; i < inputs.Length; ++i) {
+            InputActionBindings inp = inputs[i];
+            InputAction ia = FindAction(inputActionAsset, inp.name, inp.type);
+            if (ia == null) {
+                Debug.Log("Missing " + inp.name + " (" + inp.type + ")");
+            } else {
+                if (enable) {
+                    inp.BindAction(ia);
+                } else {
+                    inp.UnbindAction(ia);
+                }
+            }
+        }
+        inputActionAsset.FindActionMap("Player").Enable();
+        initialized = true;
+    }
+    private void OnEnable() {
+        if (!initialized) return;
+        BindDirectlyWithoutPlayerInput(inputBindings, true);
+    }
+    private void OnDisable() {
+        if (!initialized) return;
+        BindDirectlyWithoutPlayerInput(inputBindings, false);
+    }
+
+    public static InputAction FindAction(InputActionAsset actionAsset, string actionName, string actionInputType) {
+        foreach (var actionMap in actionAsset.actionMaps) {
+            string n = "/" + actionName;
+            foreach (var action in actionMap.actions) {
+                if (action.name == actionName || action.name.Contains(n)) {
+                    if (action.expectedControlType != actionInputType) {
+                        Debug.Log("found " + actionName + ", but Input type is " + action.expectedControlType + ", not " + actionInputType);
+                    } else {
+                        return action;
+                    }
+                }
+            }
+            //foreach (var action in actionMap.actions) { Debug.Log(actionName+"???\n"+ actionMap.actions.JoinToString("\n",a=>a.name)); }
+        }
+        return null;
+    }
 
     void BindMouselookInputMapToButton(InputAction.CallbackContext context) {
         switch (context.phase) {
@@ -102,11 +140,17 @@ public class CharacterInputAutomate : MonoBehaviour {
         }
     }
 
-    struct Inpt {
+    [Serializable]
+    public class UnityInputEvent : UnityEvent<InputAction.CallbackContext> { }
+
+    [Serializable]
+    public class InputActionBindings {
         public string name, type;
         public EventBind evnt;
-        public Inpt(string n, string t, EventBind e) {
+        public UnityInputEvent startPerformCancel = new UnityInputEvent();
+        public InputActionBindings(string n, string t, EventBind e) {
             name = n; type = t; evnt = e;
+            e.Bind(startPerformCancel);
         }
         public bool BindAction(PlayerInput playerInput) {
             if (playerInput.actions != null) {
@@ -115,188 +159,32 @@ public class CharacterInputAutomate : MonoBehaviour {
                 foreach (var e in playerInput.actionEvents) {
                     //Debug.Log("~~~~ " + e.actionName + " vs " + name);
                     if (e.actionName.Contains(n)) {
-                        Debug.Log("binding " + evnt.target + "." + evnt.methodName + " to " + e);
-                        evnt.Bind(e);
+                        Debug.Log(name+" binding {" + evnt + "()} to " + e);
+                        //evnt.Bind(e);
+                        e.AddListener(startPerformCancel.Invoke);
                         return true;
                     }
                 }
             }
             return false;
         }
+        public void BindAction(InputAction ia) {
+            if (startPerformCancel != null) {
+                ia.started  -= startPerformCancel.Invoke;
+                ia.performed-= startPerformCancel.Invoke;
+                ia.canceled -= startPerformCancel.Invoke;
+                ia.started  += startPerformCancel.Invoke;
+                ia.performed+= startPerformCancel.Invoke;
+                ia.canceled += startPerformCancel.Invoke;
+            }
+        }
+        public void UnbindAction(InputAction ia) {
+            if (startPerformCancel != null) {
+                ia.started  -= startPerformCancel.Invoke;
+                ia.performed-= startPerformCancel.Invoke;
+                ia.canceled -= startPerformCancel.Invoke;
+            }
+        }
     }
 }
 #endif
-
-
-/*
-#if !ENABLE_INPUT_SYSTEM
-public class CharacterInputAutomate : CharacterInputLegacy { }
-#else
-using UnityEngine.InputSystem;
-public class CharacterInputAutomate : MonoBehaviour {
-
-    [Tooltip("Where the player should get input from. If null (expected), will generate PlayerInput script here.")]
-    public PlayerInput playerInput;
-    [Tooltip("The Character's Controls")]
-    public InputActionAsset inputActionAsset;
-    CharacterMove cm;
-    void Start() {
-        cm = GetComponent<CharacterMove>();
-        RequiredMoves(playerInput, true, true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    struct Inpt {
-        public string name, type;
-        public EventBind evnt;
-        public System.Action<InputAction.CallbackContext> startAct, cancelAct;
-        public string[] possibleBindings;
-        public Inpt(string n, string t, EventBind e,
-            System.Action<InputAction.CallbackContext> sAct, System.Action<InputAction.CallbackContext> cAct, string[] b) {
-            name = n;type = t;evnt = e;startAct = sAct;cancelAct = cAct;possibleBindings = b;
-        }
-        public void SetupAction(InputAction ia, PlayerInput playerInput) {
-            if (playerInput == null) {
-                if (ia == null) { return; }
-                if (startAct != null) {
-                    ia.started += startAct;
-                }
-                if (cancelAct != null) {
-                    ia.canceled += cancelAct;
-                }
-            } else {
-                if (playerInput.actions != null) {
-                    Debug.Log("!!!!! player assign " + name + " " + playerInput.actionEvents.Count);
-                    string n = "/" + name;
-                    foreach (var e in playerInput.actionEvents) {
-                        Debug.Log("~~~~ " + e.actionName + " vs " + name);
-                        if (e.actionName.Contains(n)) {
-                            Debug.Log("binding " + evnt.target + "." + evnt.methodName + " to " + e);
-                            evnt.Bind(e);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    void SetMove(InputAction.CallbackContext context) {
-        Debug.Log("    "+context);
-        cm.SetMove(context);
-    }
-    public bool RequiredMoves(PlayerInput playerInput, bool alsoAssign, bool generateIfMissing) {
-        InputActionAsset iaa;
-        if (playerInput.actions == null) {
-            Debug.Log("creating new InputActionAsset");
-            iaa = InputActionAsset.CreateInstance<InputActionAsset>();
-            //playerInput.actions = iaa;
-            playerInput.notificationBehavior = PlayerNotifications.InvokeUnityEvents;
-        } else {
-            iaa = playerInput.actions;
-        }
-        Inpt[] inputs = new Inpt[] {
-            new Inpt("Move", "Vector2", new EventBind(cm, nameof(cm.SetMove)), cm.SetMove, cm.SetMove,new string[]{ "<Gamepad>/leftStick", "<XRController>/{Primary2DAxis}", "<Joystick>/stick" }),//cm.SetMove, cm.SetMove),
-  //          new Inpt("Jump", "", new EventBind(cm, nameof(cm.SetJump)), cm.SetJump, cm.SetJump)
-        };
-        Debug.Log(inputs.Length);
-        InputActionMap generated = null;
-        // generate actions in action map
-        for (int i = 0; i < inputs.Length; ++i) {
-            Inpt inp = inputs[i];
-            InputAction ia = FindAction(iaa, inp.name, inp.type);
-            if (ia == null) {
-                Debug.LogWarning("missing " + inp.name);
-                if (generateIfMissing) {
-                    if (generated == null) {
-                        generated = iaa.AddActionMap("generated");
-                        Debug.Log("generated " + generated.name);
-                    }
-                    ia = generated.AddAction(inp.name, string.IsNullOrEmpty(inp.type) ? InputActionType.Button : InputActionType.Value, inp.type);
-                    if (inp.possibleBindings != null) {
-                        for (int b = 0; b < inp.possibleBindings.Length; ++b) {
-                            Debug.Log("binding " + inp.name + " " + inp.possibleBindings[b]);
-                            ia.AddBinding(inp.possibleBindings[b]);
-                        }
-                    }
-                }
-            }
-        }
-        //if (generateIfMissing && alsoAssign && iaa != playerInput.actions) {
-            playerInput.actions = iaa;
-        //}
-        Debug.Log("####" + playerInput.actionEvents.Count);
-        // bind actions in PlayerInput
-        for (int i = 0; i < inputs.Length; ++i) {
-            Inpt inp = inputs[i];
-            InputAction ia = FindAction(iaa, inp.name, inp.type);
-            if (ia != null) {
-                if (alsoAssign) {
-                    inp.SetupAction(ia, playerInput);
-                }
-            }
-        }
-        //GenerateAndAssignActions(playerInput, inputs, ref generated, alsoAssign, generateIfMissing, iaa);
-        //if (generateIfMissing && alsoAssign && iaa != playerInput.actions) {
-        //    playerInput.actions = iaa;
-        //    GenerateAndAssignActions(playerInput, inputs, ref generated, alsoAssign, generateIfMissing, iaa);
-        //}
-
-        return true;
-    }
-
-    bool GenerateAndAssignActions(PlayerInput playerInput, Inpt[] inputs, ref InputActionMap generated, bool alsoAssign, bool generateIfMissing, InputActionAsset iaa) {
-        for (int i = 0; i < inputs.Length; ++i) {
-            Inpt inp = inputs[i];
-            InputAction ia = FindAction(iaa, inp.name, inp.type);
-            if (ia != null) {
-                if (alsoAssign) {
-                    inp.SetupAction(ia, playerInput);
-                }
-            } else {
-                Debug.LogWarning("missing " + inp.name);
-                if (generateIfMissing) {
-                    if (generated == null) {
-                        generated = iaa.AddActionMap("generated");
-                        Debug.Log("generated " + generated.name);
-                    }
-                    ia = generated.AddAction(inp.name, string.IsNullOrEmpty(inp.type) ? InputActionType.Button : InputActionType.Value, inp.type);
-                    if (inp.possibleBindings != null) {
-                        for (int b = 0; b < inp.possibleBindings.Length; ++b) {
-                            Debug.Log("binding " + inp.name + " " + inp.possibleBindings[b]);
-                            ia.AddBinding(inp.possibleBindings[b]);
-                        }
-                    }
-                    if (alsoAssign) {
-                        inp.SetupAction(ia, playerInput);
-                    }
-                } else {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-
-    public static InputAction FindAction(InputActionAsset actionAsset, string actionName, string actionInputType) {
-        foreach(var actionMap in actionAsset.actionMaps) {
-            foreach(var action in actionMap.actions) {
-                if(action.name == actionName) {
-                    if (action.expectedControlType != actionInputType) {
-                        Debug.Log("found " + actionName + ", but Input type is " + action.expectedControlType + ", not " + actionInputType);
-                    } else {
-                        return action;
-                    }
-                }
-            }
-        }
-        return null;
-    }
-}
-#endif
-*/
--- Scripts/NonStandardUnity/Character/CharacterInputLegacy.cs
+++ Scripts/NonStandardUnity/Character/CharacterInputLegacy.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 
 public class CharacterInputLegacy : MonoBehaviour {
     CharacterMove cm;
+    CharacterCamera cc;
     void Start() {
         cm = GetComponent<CharacterMove>();
+        cc = CharacterCamera.FindCameraTargettingChildOf(cm.transform);
         if (cm == null) { enabled = false; Debug.LogWarning("Missing "+nameof(CharacterMove)+" on "+name); }
     }
     void Update() {
-        Update(cm);
+        UpdateCharacterMove(cm);
+        if (cc != null && Input.GetMouseButton(1)) {
+            cc.ProcessLookRotation(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+        }
     }
 
-    public static void Update(CharacterMove cm) {
+    public static void UpdateCharacterMove(CharacterMove cm) {
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         bool jump = Input.GetButton("Jump");
         cm.MoveInput = input;
